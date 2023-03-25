30bc47ece8637a6c7801ffb65c70261c3cbb9097
Farhan <farhanshokat@gmail.com>
2023-03-26 01:59:52 +0500

    Basic Component Setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from "../assets/Logo.svg";
+
+const Header = () => {
+    return(
+        <header>
+            <img src={Logo} />
+        </header>
+    )
+}
+
+export default Header;
